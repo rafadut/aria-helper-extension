8721cc9eb9e105a0d55cfe121b26e62a7e782368
Rafael Dutra <rafaeldut89@gmail.com>
2018-01-25 21:10:31 -0200

    1.0

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using System.Web.Routing;
 
 namespace WebApplication1.Controllers
 {
@@ -25,6 +28,22 @@ namespace WebApplication1.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+    }
+
+    public static class Class1
+    {
+        public static IHtmlString ARIATextBoxFor<TModel, TProperty>(this HtmlHelper<TModel>
+helper, Expression<Func<TModel, TProperty>> exp)
+        {
+            ModelMetadata metadata = ModelMetadata.FromLambdaExpression(exp,
+            helper.ViewData);
+            var attr = new RouteValueDictionary();
+            if (metadata.IsRequired)
+            {
+                attr.Add("aria-required", true);
+            }
+            return helper.TextBoxFor(exp, attr);
         }
     }
 }
